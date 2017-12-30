e83a00f68908e7a3e8b7e297333f01e1ec3f467c
hashimshafiq <hashimshafiq0@gmail.com>
2017-12-30 23:52:16 +0500

    feeching data from api

--- app/src/main/java/weather/hashim/com/showweather/MainActivity.java
+++ app/src/main/java/weather/hashim/com/showweather/MainActivity.java
@@ -1,7 +1,9 @@
 package weather.hashim.com.showweather;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -14,7 +16,11 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
@@ -22,6 +28,8 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private final String URL = "https://api.openweathermap.org/data/2.5/weather?id=2172797&units=metric&APPID=7f4af2564fbd4a95779c2c1d55e83fcf";
 
     //Widgets
     TextView mainTemp;
@@ -49,26 +57,13 @@ public class MainActivity extends AppCompatActivity {
         weatherImage = findViewById(R.id.weatherImage);
         customList = findViewById(R.id.customList);
 
-        Model m = new Model();
-        m.setTempMain("20");
-        m.setDate("19");
-        m.setDay("Wednesday");
-        m.setDescription("Rain");
-        m.setLocation("Lahore, PK");
-        m.setTempSecondry("5");
-
-
-        weatherData.add(m);
-        m.setDay("THursday");
-        weatherData.add(m);
-        m.setDay("Friday");
-        weatherData.add(m);
 
 
 
-        arrayAdapter = new weatherDataArrayAdapter(MainActivity.this,0,weatherData);
+        new GetWeatherData().execute();
 
-        customList.setAdapter(arrayAdapter);
+        //main
+
 
 
 
@@ -115,6 +110,122 @@ public class MainActivity extends AppCompatActivity {
 
             return view;
         }
+    }
+
+    /**
+     * Async task class to get json by making HTTP call
+     */
+    private class GetWeatherData extends AsyncTask<Void, Void, Void> {
+
+        ProgressDialog pDialog=null;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Showing progress dialog
+            pDialog = new ProgressDialog(MainActivity.this);
+            pDialog.setMessage("Please wait...");
+            pDialog.setCancelable(false);
+            pDialog.show();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            HttpHandler sh = new HttpHandler();
+
+            // Making a request to url and getting response
+            String jsonStr = sh.makeServiceCall(URL);
+
+
+
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+
+                    //getting name
+                    String city = jsonObj.getString("name");
+
+                    // Getting JSON Array node of weather
+                    JSONArray weatherObject = jsonObj.getJSONArray("weather");
+                    // Getting JSON Objcts from node index=0 for getting description
+                    JSONObject descriptionObject = weatherObject.getJSONObject(0);
+                    String descriptions = descriptionObject.getString("main");
+                    System.out.println("answer: "+descriptions);
+
+                    // Getting JSON Object node of main
+                    JSONObject mainObject = jsonObj.getJSONObject("main");
+                    String maintemp = mainObject.getString("temp");
+                    String minTemp = mainObject.getString("temp_min");
+
+                    // Getting JSON Object node of sys
+                    JSONObject sysObject = jsonObj.getJSONObject("sys");
+                    String country = sysObject.getString("country");
+
+                    Model m = new Model();
+                    m.setDay("Today");
+                    m.setLocation(city+", "+country);
+                    m.setTempMain(maintemp);
+                    m.setDescription(descriptions);
+                    m.setTempSecondry(minTemp);
+
+                    weatherData.add(m);
+
+
+
+
+
+
+                } catch (final JSONException e) {
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    });
+
+                }
+            } else {
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG)
+                                .show();
+                    }
+                });
+
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+//            // Dismiss the progress dialog
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+            /**
+             * Updating parsed JSON data into ListView
+             * */
+
+            arrayAdapter = new weatherDataArrayAdapter(MainActivity.this,0,weatherData);
+
+            customList.setAdapter(arrayAdapter);
+
+            mainTemp.setText(weatherData.get(0).getTempMain()+"");
+            secTemp.setText(weatherData.get(0).getTempSecondry()+"");
+            description.setText(weatherData.get(0).getDescription()+"");
+            location.setText(weatherData.get(0).getLocation()+"");
+
+        }
+
     }
 
 
